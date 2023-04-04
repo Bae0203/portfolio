24d443c26dadbda61e75329f01bf6ec7da049afe
bae0203 <gmbae06@dgsw.hs.kr>
2023-04-04 21:24:05 +0900

    FIX :: Slogan

--- src/components/Slogan.tsx
+++ src/components/Slogan.tsx
@@ -4,12 +4,21 @@ import * as S from "../styles/Slpgan.style";
 const Slogan = () => {
   return (
     <S.SloganBackground>
-      <p>TitleSlogan</p>
-      <nav>
-        <p>SubTitleSlogan</p>
-        <p>SubTitleSlogan</p>
-        <p>SubTitleSlogan</p>
-      </nav>
+      <S.Title>실패를 두려워하지않는 개발자</S.Title>
+      <S.WrapSubTitle>
+        <S.subTitle>
+          항상 성장에 대해 생각하며 <br />
+          실패를 두려워하지않는 개발자
+        </S.subTitle>
+        <S.subTitle>
+          느리지만 꾸준히,
+          <br /> 많은 경험을 기반으로 둔 개발자
+        </S.subTitle>
+        <S.subTitle>
+          팀간의 협업을 중시하며 <br />
+          끊임없이 새로운 기술을 갈구하는 개발자
+        </S.subTitle>
+      </S.WrapSubTitle>
     </S.SloganBackground>
   );
 };
--- src/styles/Slpgan.style.ts
+++ src/styles/Slpgan.style.ts
@@ -2,8 +2,25 @@ import styled from "styled-components";
 
 export const SloganBackground = styled.div`
   z-index: -1;
-  position: absolute;
+  position: relative;
   width: 100%;
-  height: 100%;
-  background-color: #d2d2d2;
+  text-align: center;
+`;
+
+export const Title = styled.p`
+  padding-top: 20rem;
+  font-size: 3.375rem;
+  letter-spacing: 0.1em;
+  font-weight: 500;
+`;
+
+export const subTitle = styled.p`
+  margin-top: 5rem;
+  font-size: 1.2rem;
+  letter-spacing: 0.4em;
+  line-height: 2rem;
+`;
+
+export const WrapSubTitle = styled.nav`
+  margin-bottom: 20rem;
 `;
