10494c8d1fe7b8d51318698d2234aa9432813318
bae0203 <gmbae06@dgsw.hs.kr>
2023-04-29 20:25:23 +0900

    ADD :: Scroll Event

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as S from "../styles/Header.style";
 
 const Header = () => {
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll); //clean up
+    };
+  }, []);
+
+  const handleScroll = () => {
+    //console.log(window.scrollY);
+  };
   return (
     <S.Header isSlogan={true}>
       <S.TitleContext>배경민의 포트폴리오</S.TitleContext>
