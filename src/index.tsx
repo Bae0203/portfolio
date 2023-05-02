e31b6e734e1d582a77ec4019da92d99d2c3be719
bae0203 <gmbae06@dgsw.hs.kr>
2023-05-02 20:55:55 +0900

    ADD :: Recoil

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import reportWebVitals from "./reportWebVitals";
 import Router from "./Router";
+import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Router />
+    <RecoilRoot>
+      <Router />
+    </RecoilRoot>
   </React.StrictMode>
 );
 
