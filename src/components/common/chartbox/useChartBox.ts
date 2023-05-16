7b13ab089eed8445a9fdae7ded8231617a350fac
bae0203 <gmbae06@dgsw.hs.kr>
2023-05-16 18:21:09 +0900

    ADD :: import Svg

---
+++ src/components/common/chartbox/useChartBox.ts
@@ -0,0 +1,5 @@
+import Highthon from "../../../asset/Project/highthon.svg";
+import Kosamidas from "../../../asset/Project/kosamidas.svg";
+import PlusBtn from "../../../asset/Project/PlusBtn.svg";
+
+export { Highthon, Kosamidas, PlusBtn };
