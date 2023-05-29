509da0f7e636b30428de74d81d0e5cb77ee5affa
bae0203 <gmbae06@dgsw.hs.kr>
2023-05-29 18:23:46 +0900

    ADD :: Key props

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,15 +3,17 @@ import FooterValue from "./FooterValue.json";
 import FooterContents from "./FooterContents";
 import * as S from "../../styles/Footer.style";
 import ShortCutBtn from "./ShortCutBtn";
+import SvgImage from "./useShortCutSvg";
 
 const Footer = () => {
   return (
     <S.FooterContainer>
       <S.MainContainer>
         <S.ContentsContainer>
-          {FooterValue.contentValue.map((value) => {
+          {FooterValue.contentValue.map((value, index) => {
             return (
               <FooterContents
+                key={index}
                 title={value.title}
                 context={{
                   title: value.content.title,
@@ -23,7 +25,13 @@ const Footer = () => {
         </S.ContentsContainer>
         <S.ShortCutContainer>
           {FooterValue.ShortCutBtn.map((value, index) => {
-            return <ShortCutBtn Link={value} SvgImage="" key={index} />;
+            return (
+              <ShortCutBtn
+                Link={value}
+                SvgImage={SvgImage[index]}
+                key={index}
+              />
+            );
           })}
         </S.ShortCutContainer>
       </S.MainContainer>
