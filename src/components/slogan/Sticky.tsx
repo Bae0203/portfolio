d0fea3e686ce8f1a2c4000f37b20f0284ddfaf1d
bae0203 <gmbae06@dgsw.hs.kr>
2023-07-09 22:09:38 +0900

    Fix :: Sticky code

--- src/components/slogan/Sticky.tsx
+++ src/components/slogan/Sticky.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as S from "./Sticky.style";
 
 interface ISloganProps {
@@ -9,17 +9,44 @@ interface ISloganProps {
 const Sticky = (props: ISloganProps) => {
   const parentsRef = useRef<HTMLDivElement>(null);
   const childRef = useRef<HTMLDivElement>(null);
+  const [opacity, setOpacity] = useState<number>(0);
+
+  useEffect(() => {
+    const ComponentElement = parentsRef.current;
+    if (!ComponentElement) return;
+    const componentRect = ComponentElement.getBoundingClientRect();
+    const handleScroll = () => {
+      const scrollTop = document.documentElement.scrollTop;
+      const parentElement = parentsRef.current;
+      const childElement = childRef.current;
+      if (!parentElement || !childElement) return;
+
+      const parentRect = parentElement.getBoundingClientRect();
+      const parentHeight = parentRect.height;
+      const childRect = childElement.getBoundingClientRect();
+      const childHeight = childRect.height;
+      const MinHeight: number = componentRect.top + childHeight; //현재 높이 + 자신의 높이
+      const MaxHeight: number = componentRect.top + parentHeight; //현재높이
+
+      const scrollPercent = MaxHeight - MinHeight;
+      console.log(props.title, childRect.top, parentRect.top, scrollTop);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <S.StickyContainer>
-      <S.Sticky ref={parentsRef}>
-        <div ref={childRef}>
+    <S.StickyContainer ref={parentsRef}>
+      <S.Sticky ref={childRef}>
+        <div>
           {props.title ? (
             <S.Title>{props.title}</S.Title>
           ) : (
             props.context && (
               <S.WrapSubTitle>
-                {props.context.map((e) => {
-                  return <S.subTitle>{e}</S.subTitle>;
+                {props.context.map((e, i) => {
+                  return <S.subTitle key={i}>{e}</S.subTitle>;
                 })}
               </S.WrapSubTitle>
             )
